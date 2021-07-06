bdb3ee03b899e204aea0d21903e5a5050e960186
Janet Iqal <j.iqal35@gmail.com>
2021-07-06 16:24:55 -0500

    added id's to html for each timeslot, created a check hour function

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,42 @@
 
 var currentDay = document.getElementById("currentDay");
-todaysDate= moment().format('MMMM Do YYYY');
-currentDay.innerHTML =todaysDate;
+todaysDate = moment().format('MMMM Do YYYY');
+currentDay.innerHTML = todaysDate;
 var currentTime = document.getElementById("currentTime");
-time = moment().format('LTS');
-currentTime.innerHTML =time;
+time = moment().hour()
+currentTime.innerHTML = time;
 
 // click event for save button & local storage
-$("button").click(function(){
+$("button").click(function () {
     console.log("button clicked")
     var inputText = $("textarea").val();
     console.log(inputText)
     localStorage.setItem("input", inputText);
 });
+
+//create color coding system for current hour, add class 
+//for loop? function compares currrent hour to the index of page
+function checkHour() {
+    const militaryHours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
+    const currentHour = moment().hour()
+    
+    for (let i = 0; i < militaryHours.length; i++) {
+        console.log(militaryHours[i])
+        console.log(currentHour)
+        if (currentHour === militaryHours[i]) {
+            $("textarea").addClass("present")
+        } else if (currentHour < militaryHours[i]) {
+            $("textarea").addClass("future")
+        } else (currentHour > militaryHours[i])
+            $("textarea").addClass("past")
+    }
+};
+checkHour();
+
+// const timeOptions =["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"]
+// var timeSlots = document.getElementsByClassName("hour");
+// let index= 0;
+// for (let i = 0 ; i < timeOptions.length; i ++){
+//     if(timeOptions[i]===timeSlots[index])
+//     console.log("what is this?")
+// }
