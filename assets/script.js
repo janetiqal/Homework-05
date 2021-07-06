f66acec6c4495547378121b26b47d33b6328dcd7
Janet Iqal <j.iqal35@gmail.com>
2021-07-06 12:48:24 -0500

    added the rest of the time windows, on click event created w jquery

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
 
-var currentDay = document.getElementById("currentDay")
-currentTime= moment().format('MMMM Do YYYY')
-currentDay.innerHTML =currentTime;
+var currentDay = document.getElementById("currentDay");
+todaysDate= moment().format('MMMM Do YYYY');
+currentDay.innerHTML =todaysDate;
+var currentTime = document.getElementById("currentTime");
+time = moment().format('LTS');
+currentTime.innerHTML =time;
 
 var timeRightNow=moment().subtract
+//click event for save button
+$("button").click(function(){
+    console.log("button clicked")
+});
