46b9221c68f4fa63aee50c020345859e2472e2e8
Janet Iqal <j.iqal35@gmail.com>
2021-07-13 14:53:16 -0500

    added comments in javascript file

--- assets/script.js
+++ assets/script.js
@@ -21,9 +21,9 @@ $("button").click(function (event) {
 });
 
 //INDEX through them/for loop instead of doing it individually.
-const divID = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+// const divID = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 
-
+//gettting textarea value from local storage
 $("#9AM .description").val(localStorage.getItem("9AM"));
 $("#10AM .description").val(localStorage.getItem("10AM"));
 $("#11AM .description").val(localStorage.getItem("11AM"));
@@ -34,12 +34,12 @@ $("#3PM .description").val(localStorage.getItem("3PM"));
 $("#4PM .description").val(localStorage.getItem("4PM"));
 $("#5PM .description").val(localStorage.getItem("5PM"));
 //create color coding system for current hour, add class 
-//for loop? function compares currrent hour to the index of page
-//
+//function compares currrent hour to the index of page
+
 function checkHour() {
     const militaryHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
     const currentHour = moment().hour();
-    //added ID's to each individual textarea to correspond to military hours index. looping through the array at the same time were also checking the ID of the text area to identify it 
+    //added ID's to each individual textarea to correspond to military hours index. looping through the array at the same time I am also checking the ID of the text area to identify it 
     for (let i = 0; i < militaryHours.length; i++) {
         console.log(militaryHours[i])
         console.log(currentHour)
@@ -60,22 +60,6 @@ console.log($("textarea"))
 checkHour();
 });
 
-
-
-
-
-
-// function check9am (){
-//     const currentHour= moment().hour();
-//     if (currentHour === 9){
-//         $("#9").addClass("present");
-//     }else if (currentHour < 9){
-//         $("#9").addClass("future");
-//     }else if (currentHour>9){
-//         $("#9").addClass("past");
-//     }
-
-// }check9am();
 
 
 
